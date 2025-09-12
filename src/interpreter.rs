c5b8b596a341c531e709afd790d7ad82b1adcd2f
Olivier Lasne <lasneolivier@gmail.com>
2025-09-12 21:39:26 +0200

    created an open file function

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,8 +1,25 @@
-use std::path::PathBuf;
+use std::{fs::File, io::BufReader, path::PathBuf};
 // pub mod interpreter;
 
-pub fn binary_template(template: PathBuf, file: PathBuf) {
+fn open_file(filepath: &PathBuf) -> Result<BufReader<File>, std::io::Error> {
+    let file = File::open(filepath)?;
+    let reader = BufReader::new(file);
+    Ok(reader)
+}
+
+pub fn binary_template(template_path: PathBuf, file_path: PathBuf) {
     println!("Hi mom!");
 
-    
+    let template = open_file(&template_path)
+        .map_err(|_| {
+            eprintln!("Failed to open {}", &template_path.display());
+            return;
+        });
+
+    let file = open_file(&file_path)
+        .map_err(|_| {
+            eprintln!("Failed to open {}", &file_path.display());
+            return;
+        });
+
 }
