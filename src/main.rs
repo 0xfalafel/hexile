87ce477ef3139a799c5cae388acf895af8b9ad1a
Olivier Lasne <lasneolivier@gmail.com>
2025-09-12 19:28:26 +0200

    inital commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::path::PathBuf;
+use std::fs;
+use clap::Parser;
+
+#[derive(Parser,Default,Debug)]
+//#[command(author, version, about, long_about = None)]
+//#[command(propagate_version = true)]
+struct Cli {
+    template: PathBuf,
+    
+    file: PathBuf,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    println!("Hello, world!");
+}
